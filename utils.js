e18d30e670b9772998d1a05ed9d9d8ad39844457
Franklin Chieze <chieze.franklin@gmail.com>
2017-04-20 09:22:01 +0100

    corrected a stupid error

--- utils.js
+++ utils.js
@@ -2,26 +2,24 @@ var fs = require("fs");
 var path = require("path");
 
 module.exports = {
-	Misc : {
-		getCurrentQuestion: function(userid, callback) {
-			//
-		},
-		getRandomQuestion: function(subjid, callback) {
-			fs.readFile(path.join(__dirname, 'data', subjid + '.json'), function (err, data) {
-				if(err){
-					callback(err);
-				}
-				
-				var paper = JSON.parse(data);
-				//get a random number btwn 0 and (paper.length+1)
-				var index = Math.floor(Math.random() * paper.length);
-				var q = paper[index];
+	getCurrentQuestion: function(userid, callback) {
+		//
+	},
+	getRandomQuestion: function(subjid, callback) {
+		fs.readFile(path.join(__dirname, 'data', subjid + '.json'), function (err, data) {
+			if(err){
+				callback(err);
+			}
+			
+			var paper = JSON.parse(data);
+			//get a random number btwn 0 and (paper.length+1)
+			var index = Math.floor(Math.random() * paper.length);
+			var q = paper[index];
 
-				callback(null, q);
-			});
-		},
-		setCurrentSubject: function(userid, subjectid, callback) {
-			//
-		}
+			callback(null, q);
+		});
+	},
+	setCurrentSubject: function(userid, subjectid, callback) {
+		//
 	}
 };
